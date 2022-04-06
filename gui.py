1c43c95a8ea8c8f5e4c603eac7ec91281ba12b85
Alon Kahana <alonkahana@mail.tau.ac.il>
2022-04-06 20:11:38 +0300

    few tweaks

--- gui.py
+++ gui.py
@@ -1,97 +1,111 @@
 import PySimpleGUI as sg
 
-# from main import ep_chassis
-# from main import get_nway_dis
+from main import*
 
 """
-Demonstrates using a "tight" layout with a Dark theme.
-Shows how button states can be controlled by a user application.  The program manages the disabled/enabled
-states for buttons and changes the text color to show greyed-out (disabled) buttons
+Basic GUI for Robomaster 
 """
 
-sg.theme('Dark')
-# sg.set_options(element_padding=(3, 2))
 
-row_1 = sg.Frame('Movement:',
-                 [[sg.Text('X-Axis(m):', pad=((10, 3), 30)), sg.Input(size=(6, 1), default_text='.3', key='-X-',
-                                                                      background_color='white', text_color='black'),
-                   sg.Text('Y-Axis(m):', pad=((10, 3), 0)), sg.Input(size=(6, 1), default_text='.3', key='-Y-',
-                                                                     background_color='white', text_color='black'),
-                   sg.Text('Rotation(deg):', pad=((10, 3), 0)),
-                   sg.Input(size=(6, 1), default_text='90', key='-ROTATION_DEG-',  # enable_events
-                            background_color='white', text_color='black')]])
+def run_gui():
+    sg.theme('Dark')
+    # sg.set_options(element_padding=(3, 2))
 
-row_2 = sg.Frame('Speed:', [[sg.Text('XY-Speed(m/s) [0.5,2]:', pad=((10, 3), 20)),
-                             sg.Input(size=(10, 1), default_text='1', key='-XY_SPEED-',
-                                      background_color='white', text_color='black'),
-                             sg.Text('Rotation(deg/sec) [10, 540]:', pad=((10, 3), 0)),
-                             sg.Input(size=(10, 1), default_text='30', key='-ROTATION_SPEED-',
-                                      background_color='white', text_color='black')]])
+    row_1 = sg.Frame('Movement:',
+                     [[sg.Text('X-Axis(m):', pad=((10, 3), 25)), sg.Input(size=(6, 1), default_text='.3', key='-X-', enable_events=True,
+                                                                          background_color='white', text_color='black'),
+                       sg.Text('Y-Axis(m):', pad=((10, 3), 0)), sg.Input(size=(6, 1), default_text='.3', key='-Y-',
+                                                                         background_color='white', text_color='black'),
+                       sg.Text('Rotation(deg):', pad=((10, 3), 0)),
+                       sg.Input(size=(6, 1), default_text='90', key='-ROTATION_DEG-',
+                                background_color='white', text_color='black')]])
 
-row_3 = sg.Frame('Sensor Freq(Hz):', [[sg.Radio('1', 'radio1', default=True, key='-1hz-', size=(3, 1)),
-                                       sg.Radio('5', 'radio1', key='-5hz-', size=(3, 1)),
-                                       sg.Radio('10', 'radio1', key='-10hz-', size=(3, 1)),
-                                       sg.Radio('20', 'radio1', key='-20hz-', size=(3, 1)),
-                                       sg.Radio('50', 'radio1', key='-50hz-', size=(3, 1))]], )
+    row_2 = sg.Frame('Speed:', [[sg.Text('XY-Speed(m/s) [0.5,2]:', pad=((10, 3), 20)),
+                                 sg.Input(size=(10, 1), default_text='1', key='-XY_SPEED-',
+                                          background_color='white', text_color='black'),
+                                 sg.Text('Rotation(deg/sec) [10, 540]:', pad=((10, 3), 0)),
+                                 sg.Input(size=(10, 1), default_text='30', key='-ROTATION_SPEED-',
+                                          background_color='white', text_color='black')]])
 
-row_4 = sg.Button('Go', key='-GO-', size=(10, 3))
+    row_3 = sg.Frame('Sensor Freq(Hz):',
+                     [[sg.Radio('1', 'Hz', default=True, key='-1hz-', size=(3, 1), enable_events=True),
+                       sg.Radio('5', 'Hz', key='-5hz-', size=(3, 1), enable_events=True),
+                       sg.Radio('10', 'Hz', key='-10hz-', size=(3, 1), enable_events=True),
+                       sg.Radio('20', 'Hz', key='-20hz-', size=(3, 1), enable_events=True),
+                       sg.Radio('50', 'Hz', key='-50hz-', size=(3, 1), enable_events=True),
+                       sg.Text('Distance = ', key='-DIST-', size=(15, 1))]], )  # make in cm
+    chosen_freq = 1
 
-col_1 = sg.Frame('Manual Control:', [[row_1],
-                              [row_2],
-                              [row_3],
-                              [row_4]])
+    row_4 = sg.Frame('', [[sg.Button('Go', key='-GO-', size=(10, 3)), sg.Button('Scan', key='-SCAN-', size=(10, 3))]],
+                     border_width=0)
 
-col_2 = sg.Frame('Functions:',
-                 [[sg.Text('n-way distance:', pad=((10, 3), 30)), sg.Input(size=(6, 1), default_text='4', key='-N-',
-                                                                      background_color='white', text_color='black'),
-                   sg.Button('Go', key='-N_SCAN-', size=(10, 3)),
-                   sg.Text('Distance = ', key='-DIST_ARR-')
-                  ],
-                  ])
+    col_1 = sg.Frame('Manual Control:', [[row_1],
+                                         [row_2],
+                                         [row_3],
+                                         [row_4]], element_justification='center')
 
-layout = [[col_1],
-          [col_2]]
+    col_2 = sg.Frame('Functions:',
+                     [[sg.Text('n-way distance:', pad=((10, 3), 30)), sg.Input(size=(6, 1), default_text='4', key='-N-',
+                                                                               background_color='white',
+                                                                               text_color='black'),
+                       sg.Button('Go', key='-N_SCAN-', size=(10, 3)),
+                       sg.Text('Output = ', key='-DIST_ARR-', size=(15, 1))
+                       ],
+                      ])
 
-window = sg.Window("Robo", layout,
-                   default_element_size=(15, 15),
-                   # text_justification='center',
-                   auto_size_text=True,
-                   auto_size_buttons=True,
-                   element_justification='center'
-                   # default_button_element_size=(12, 12),
-                   # finalize=True
-                   )
+    layout = [[col_1],
+              [col_2]]
 
-# for key, state in {'-Start-': False}.items():
-#     window[key].update(disabled=state)
+    window = sg.Window("Robo", layout,
+                       default_element_size=(15, 15),
+                       # text_justification='center',
+                       auto_size_text=True,
+                       auto_size_buttons=True,
+                       element_justification='center',
+                       finalize=True
+                       )
 
-recording = have_data = False
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED:
-        break
-    # if event == '-GO-':
-    # for key, state in {'-X-': 0, '-Y-': 0}.items():
-    #     window[key] = state
-    # recording = True
-    # ep_chassis.move(window['-X-'], window['-Y-'], window['-ROTATION_DEG-'], window['-XY_SPEED-'], window['-ROTATION_SPEED-']).wait_for_completed()
-    # if event == '-N_SCAN-':
-    #     window['-DIST_ARR-'].update(value='test') # get_nway_dis(window['N'])
+    # for key, state in {'-Start-': False}.items():
+    #     window[key].update(disabled=state)
 
-    # elif event == '-Stop-' and recording:
-    #     [window[key].update(disabled=value) for key, value in {
-    #         '-Start-': False, '-Stop-': True, '-Reset-': False, '-Submit-': False}.items()]
-    #     recording = False
-    #     have_data = True
-    # elif event == '-Reset-':
-    #     [window[key].update(disabled=value) for key, value in {
-    #         '-Start-': False, '-Stop-': True, '-Reset-': True, '-Submit-': True}.items()]
-    #     recording = False
-    #     have_data = False
-    # elif event == '-Submit-' and have_data:
-    #     [window[key].update(disabled=value) for key, value in {
-    #         '-Start-': False, '-Stop-': True, '-Reset-': True, '-Submit-': False}.items()]
-    #     recording = False
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == '-GO-':
+            move_chassi(float(values['-X-']), float(values['-Y-']), float(values['-ROTATION_DEG-']), float(values['-XY_SPEED-']),
+                        float(values['-ROTATION_SPEED-']))
+        elif event == '-N_SCAN-':
+            window['-DIST_ARR-'].update(value=str(get_nway_dis(values['N'])))
+        elif event == '-SCAN-':
+            window['-DIST-'].update(value=average_dis(1))
+        elif event == '-1hz-':
+            chosen_freq = 1
+        elif event == '-5hz-':
+            chosen_freq = 5
+        elif event == '-10hz-':
+            chosen_freq = 10
+        elif event == '-20hz-':
+            chosen_freq = 20
+        elif event == '-50hz-':
+            chosen_freq = 50
+        # elif event == ep_chassis.move(window['-X-'], window['-Y-'], window['-ROTATION_DEG-'], window['-XY_SPEED-'], window['-ROTATION_SPEED-']).wait_for_completed()'-Stop-' and recording:
+        #     [window[key].update(disabled=value) for key, value in {
+        #         '-Start-': False, '-Stop-': True, '-Reset-': False, '-Submit-': False}.items()]
+        #     recording = False
+        #     have_data = True
+        # elif event == '-Reset-':
+        #     [window[key].update(disabled=value) for key, value in {
+        #         '-Start-': False, '-Stop-': True, '-Reset-': True, '-Submit-': True}.items()]
+        #     recording = False
+        #     have_data = False
+        # elif event == '-Submit-' and have_data:
+        #     [window[key].update(disabled=value) for key, value in {
+        #         '-Start-': False, '-Stop-': True, '-Reset-': True, '-Submit-': False}.items()]
+        #     recording = False
 
-window.close()
+    window.close()
+
+
+run_gui()
--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 from robomaster import robot
+import PySimpleGUI as sg
+
+from gui import*
+
 
 ep_robot = robot.Robot()
+
 connected = ep_robot.initialize(conn_type="ap")
 
+ep_chassis = ep_robot.chassis
+ep_version = ep_robot.get_version()
+print("Robot Version: {0}".format(ep_version))
+
+
 if connected:
-    print("connected")
+    run_gui()
+    quit = False
+    while not quit:
+        inp = input("type q to quit: ").strip()
+        if inp == "q":
+            quit = True
+            ep_robot.close()
+
 else:
     print("ERROR - failed to connect")
 
-ep_chassis = ep_robot.chassis
 
 def get_nway_dis(n):
     dist_array = []
     for i in range(n):
-        dist_array.append(average_dis(5))
-        ep_chassis.move(0, 0, 360/n, xy_speed=1, z_speed=30).wait_for_completed()
+        dist_array.append(average_dis(5))  # 5 distance samples
+        ep_chassis.move(0, 0, 360 / n, xy_speed=1, z_speed=30).wait_for_completed()
     return dist_array
 
 
 def average_dis(num_of_readings):
     total_dis = 0
-    while num_of_readings > 0 :
+    while num_of_readings > 0:
         # ep_robot.sensor.sub_distance(freq=1, callback=lambda x: print(x))
-        cur_dis = ep_robot.sensor.sub_distance(freq=1, callback=lambda x: print(x))[0]
-        #ep_robot.unsub_distance( )
+        cur_dis = ep_robot.sensor.sub_distance(freq=1, callback=lambda x: print(x))
+        # ep_robot.unsub_distance( )
         total_dis += cur_dis
         num_of_readings -= 1
-    return total_dis/num_of_readings
+    return total_dis / num_of_readings
 
 
-print(get_nway_dis(4))
+def move_chassi(x, y, z, xy_speed, rot_speed):
+    ep_chassis.move(x, y, z, xy_speed, rot_speed).wait_for_completed()
 
-# ep_robot.close()
+
+
+
+
